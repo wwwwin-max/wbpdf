870eed4c29c7ee2826c190a7f4dca9187167d524
wwwwin-max <2607655786@qq.com>
2026-07-23 13:42:21 +0800

    Refactor access token and ticket retrieval logic

--- api/wx_config.js
+++ api/wx_config.js
@@ -1,36 +1,37 @@
 // api/wx_config.js
-// 使用 Node.js 内置模块，无需安装任何第三方依赖
-
-async function getAccessToken(appid, secret) {
-  const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  return data.access_token;
-}
-
-async function getJsapiTicket(accessToken) {
-  const url = `https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${accessToken}&type=jsapi`;
-  const res = await fetch(url);
-  const data = await res.json();
-  return data.ticket;
-}
-
 export default async function handler(req, res) {
-  const { url } = req.query;
+  let { url } = req.query;
   if (!url) {
     return res.status(400).json({ error: 'missing url' });
   }
 
+  // 规范化 URL：去掉末尾的斜杠，因为微信签名要求一致
+  url = url.replace(/\/$/, '');
+  
   const appid = process.env.WX_APPID;
   const secret = process.env.WX_SECRET;
 
   try {
-    const token = await getAccessToken(appid, secret);
-    const ticket = await getJsapiTicket(token);
+    // 获取 access_token
+    const tokenRes = await fetch(
+      `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}`
+    );
+    const tokenData = await tokenRes.json();
+    const access_token = tokenData.access_token;
+
+    // 获取 jsapi_ticket
+    const ticketRes = await fetch(
+      `https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${access_token}&type=jsapi`
+    );
+    const ticketData = await ticketRes.json();
+    const ticket = ticketData.ticket;
+
+    // 生成签名
     const noncestr = Math.random().toString(36).substr(2, 15);
     const timestamp = Math.floor(Date.now() / 1000);
-    const str = `jsapi_ticket=${ticket}&noncestr=${noncestr}&timestamp=${timestamp}&url=${url}`;
-    const signature = require('crypto').createHash('sha1').update(str).digest('hex');
+    const string = `jsapi_ticket=${ticket}&noncestr=${noncestr}&timestamp=${timestamp}&url=${url}`;
+    const crypto = require('crypto');
+    const signature = crypto.createHash('sha1').update(string).digest('hex');
 
     return res.json({
       appId: appid,
